90b3cca5e604bc25581013a7842c98335ea22677
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2025-06-28 19:23:13 +0000

    Add eraser toggle and clear grid functionality with tracked grid size

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const container = document.querySelector(".container");
 const containerSize = 512 // pixels
-
+let isEraser=false;
+let currentGridSize=16 // by default
 function generateGrid(num) {
     //clear existing squares
     container.innerHTML = '';
@@ -12,7 +13,7 @@ function generateGrid(num) {
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            (isEraser) ? square.style.backgroundColor='White': square.style.backgroundColor = 'black';
         });
         container.appendChild(square);
     }
@@ -21,14 +22,25 @@ function generateGrid(num) {
 
 
 const resizeBtn = document.querySelector("#resizeBtn");
+const eraseBtn =document.querySelector("#eraser");
+const clearBtn=document.querySelector("#clearBtn");
+
 resizeBtn.addEventListener('click', () => {
     let userInput = prompt("Enter Grid Size (max 100) : ");
     let userInputToInt = parseInt(userInput)
 
     if (userInputToInt > 0 && userInputToInt <= 100) {
+        currentGridSize=userInputToInt;
         generateGrid(userInputToInt);
     } else {
         alert("Invalid input. Using default 16x16 grid.");
         generateGrid(16);
     }
+})
+eraseBtn.addEventListener('click',()=>{
+    isEraser=!isEraser;
+    eraseBtn.textContent = isEraser ? 'Eraser: ON' : 'Eraser: OFF';
+})
+clearBtn.addEventListener('click',()=>{
+    generateGrid(currentGridSize);
 })
